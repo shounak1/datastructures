317cc3806c5fb49c475d024a8e9104280e7af4c7
Shounak Bhandekar <shounakbhandekar96@gmail.com>
2022-02-23 14:32:33 +0000

    Added code to calculate gcd

--- arrays/array-rotation.c
+++ arrays/array-rotation.c
@@ -30,6 +30,40 @@ void right_rotation(int *arr, int size_of_array, int num_of_rotations)
     }
 }
 
+void left_rotation_by_gcd(int *arr, int size_of_array, int num_of_rotations)
+{
+    int counter;
+    int gcd = 0;
+    if (size_of_array > num_of_rotations)
+    {
+        counter = num_of_rotations;
+    }
+    else if (size_of_array > num_of_rotations)
+    {
+        counter = size_of_array;
+    }
+    else
+    {
+        gcd = size_of_array;
+    }
+
+    int i = 0;
+
+    if (gcd == 0)
+    {
+        for (i = counter; i > 0; i--)
+        {
+            if (size_of_array % i == 0 && num_of_rotations % i == 0)
+            {
+                gcd = i;
+                break;
+            }
+        }
+    }
+
+    printf ("Gcd is : %d", gcd);
+}
+
 void print_array(int *arr, int size_of_array)
 {
     int i = 0;
@@ -41,8 +75,8 @@ void print_array(int *arr, int size_of_array)
 
 int main()
 {
-    int arr1[] = {1, 2, 3, 4, 5};
-    int arr2[] = {1, 2, 3, 4, 5};
+    int arr1[] = {1, 2, 3, 4, 5, 6};
+    int arr2[] = {1, 2, 3, 4, 5, 6};
 
     int rotate;
     int size_of_array = (int)sizeof(arr1) / sizeof(arr1[0]);
@@ -52,9 +86,11 @@ int main()
     scanf("%d", &rotate);
     left_rotation(arr1, size_of_array, rotate);
     right_rotation(arr2, size_of_array, rotate);
-    printf ("\nLeft Rotated Array : ");
+    printf("\nLeft Rotated Array : ");
     print_array(arr1, size_of_array);
-    printf ("\nRight Rotated Array : ");
+    printf("\nRight Rotated Array : ");
     print_array(arr2, size_of_array);
+
+    left_rotation_by_gcd(arr1, size_of_array, rotate);
     return 0;
 }
